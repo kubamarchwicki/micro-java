b4fe130c4db5f9b35735b262c11fe6084aafb2f9
kubamarchwicki <kuba@marchwicki.pl>
2014-06-24 21:46:15 +0200

    Spark example: externalized db configuration

--- examples/spark/src/main/java/pl/marchwicki/microjava/TodoMVC.java
+++ examples/spark/src/main/java/pl/marchwicki/microjava/TodoMVC.java
@@ -11,14 +11,20 @@ import pl.marchwicki.microjava.services.db.DB;
 import pl.marchwicki.microjava.services.db.TodoDAO;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import static spark.Spark.*;
 
 public class TodoMVC {
 
-    public static void main(String[] args) throws SQLException {
-        final DataSource dataSource = DB.setup(null, null, null);
+    public static void main(String[] args) throws SQLException, IOException {
+        final Properties props = new Properties();
+        props.load(TodoMVC.class.getResourceAsStream("/db.properties"));
+        final DataSource dataSource = DB.setup(props.getProperty("url"),
+                props.getProperty("user"),
+                props.getProperty("password"));
 
         //TODO: close connection?? spark lifecycle
         final DSLContext create = DSL.using(dataSource, SQLDialect.MYSQL);
--- examples/spark/src/main/java/pl/marchwicki/microjava/services/db/DB.java
+++ examples/spark/src/main/java/pl/marchwicki/microjava/services/db/DB.java
@@ -9,28 +9,10 @@ public class DB {
     public static DataSource setup(String url, String user, String password) {
 
         PoolProperties p = new PoolProperties();
-        p.setUrl("jdbc:mysql://localhost:3306/microjava");
         p.setDriverClassName("com.mysql.jdbc.Driver");
-        p.setUsername("root");
-        p.setPassword("root");
-
-        p.setJmxEnabled(true);
-        p.setTestWhileIdle(false);
-        p.setTestOnBorrow(true);
-        p.setValidationQuery("SELECT 1");
-        p.setTestOnReturn(false);
-        p.setValidationInterval(30000);
-        p.setTimeBetweenEvictionRunsMillis(30000);
-        p.setMaxActive(100);
-        p.setInitialSize(10);
-        p.setMaxWait(10000);
-        p.setRemoveAbandonedTimeout(60);
-        p.setMinEvictableIdleTimeMillis(30000);
-        p.setMinIdle(10);
-        p.setLogAbandoned(true);
-        p.setRemoveAbandoned(true);
-        p.setJdbcInterceptors("org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;" +
-                "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+        p.setUrl(url);
+        p.setUsername(user);
+        p.setPassword(password);
 
         return new org.apache.tomcat.jdbc.pool.DataSource(p);
     }
