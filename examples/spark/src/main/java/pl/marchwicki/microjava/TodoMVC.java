27bc6a5b3e3ad549c74f5bcfbd20d2bbd31dff95
kubamarchwicki <kuba@marchwicki.pl>
2014-06-24 21:30:11 +0200

    Spark example: routes

--- examples/spark/src/main/java/pl/marchwicki/microjava/TodoMVC.java
+++ examples/spark/src/main/java/pl/marchwicki/microjava/TodoMVC.java
@@ -1,8 +1,11 @@
 package pl.marchwicki.microjava;
 
+import com.google.common.base.Optional;
+import com.google.gson.Gson;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import pl.marchwicki.microjava.model.Todo;
 import pl.marchwicki.microjava.services.Store;
 import pl.marchwicki.microjava.services.db.DB;
 import pl.marchwicki.microjava.services.db.TodoDAO;
@@ -22,6 +25,35 @@ public class TodoMVC {
         final TodoDAO dao = new TodoDAO(create);
         final Store store = new Store(dao);
 
-        get("/hello", (request, response) -> store.getAll(), new JsonTransformer());
+        staticFileLocation("META-INF/resources/");
+
+        get("/todos", (request, response) -> store.getAll(), new JsonTransformer());
+
+        post("/todos", (req, resp) -> {
+            Todo data = new Gson().fromJson(req.body(), Todo.class);
+            Todo todo = store.save(data);
+
+            resp.status(200);
+            resp.header("Content-Type", "application/json");
+            return todo;
+        }, new JsonTransformer());
+
+        put("/todos/:id", (req, resp) -> {
+            long id = Long.parseLong(req.params(":id"));
+            Todo data = new Gson().fromJson(req.body(), Todo.class);
+            Optional<Todo> todo = store.save(id, data);
+
+            resp.status(204);
+            resp.header("Content-Type", "application/json");
+            return todo;
+        }, new JsonTransformer());
+
+        delete("/todos/:id", (req, resp) -> {
+            long id = Long.parseLong(req.params(":id"));
+            store.remove(id);
+
+            halt(204);
+            return null;
+        });
     }
 }
