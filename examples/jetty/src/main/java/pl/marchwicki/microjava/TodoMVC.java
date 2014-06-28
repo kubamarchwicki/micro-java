c1d0fdd72670045668bdc56cbcd530ee6b75faed
kubamarchwicki <kuba@marchwicki.pl>
2014-06-28 21:42:44 +0200

    Jetty examples: context configuration

--- examples/jetty/src/main/java/pl/marchwicki/microjava/TodoMVC.java
+++ examples/jetty/src/main/java/pl/marchwicki/microjava/TodoMVC.java
@@ -1,8 +1,6 @@
 package pl.marchwicki.microjava;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -27,32 +25,22 @@ public class TodoMVC {
 
         final Store store = new Store(new TodoDAO(ds));
 
-
-        HandlerList handlers = new HandlerList();
-
-        //servlet - initialize the servlet
-        ServletContextHandler servlet = new ServletContextHandler();
-        servlet.setContextPath("/todos");
-        ServletHolder holder = new ServletHolder();
-        holder.setServlet(new TodoMVCServlet(store));
-        servlet.addServlet(holder, "/*");
-        handlers.addHandler(servlet);
-
-        //webcontext - load webResources from jar files
         WebAppContext root = new WebAppContext();
         root.setConfigurations(new Configuration[]{
                 new WebXmlConfiguration(),
         });
-
         root.setContextPath("/");
         root.setParentLoaderPriority(true);
 
         String webDir = TodoMVC.class.getClassLoader().getResource("META-INF/resources").toExternalForm();
         root.setResourceBase(webDir);
-        handlers.addHandler(root);
+
+        ServletHolder holder = new ServletHolder();
+        holder.setServlet(new TodoMVCServlet(store));
+        root.addServlet(holder, "/todos/*");
 
         Server server = new Server(8080);
-        server.setHandler(handlers);
+        server.setHandler(root);
         server.start();
         server.join();
     }
